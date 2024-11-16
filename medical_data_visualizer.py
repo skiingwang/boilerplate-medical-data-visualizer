1cc1ac06dd27ce0500e2055ef4a73bc90c44e943
Yvan Wang <skiingwang@163.com>
2024-11-16 10:01:24 +0000

    done

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,31 +4,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['overweight'] = (df['weight']/(df['height']/100)**2 > 25).astype(np.int8)
 
 # 3
+df['cholesterol'] = df['cholesterol'].gt(1).astype(np.int8)
 
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = pd.melt(df, id_vars=['id'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'], var_name='variable', value_name='value')
 
 
     # 6
-    df_cat = None
+    df_cat1 = df.groupby(['cardio']).count()
     
 
     # 7
-
+    long_df = pd.melt(df_cat1, id_vars=['id'], value_vars=['age', 'sex', 'height', 'weight', 'ap_hi', 'ap_lo', 'cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'], var_name='variable', value_name='value')
 
 
     # 8
-    fig = None
-
+    fig = sns.scatterplot(data=df_cat, x='value')
 
     # 9
     fig.savefig('catplot.png')
